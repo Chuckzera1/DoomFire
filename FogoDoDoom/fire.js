51a9105ddd5e68d10aed173733265f6383fbce2c
Chuckzera1 <43689228+Chuckzera1@users.noreply.github.com>
2019-03-14 20:22:45 -0300

    Add files via upload

---
+++ FogoDoDoom/fire.js
@@ -0,0 +1,133 @@
+const firePixelsArray = []
+const fireWidth = 40
+const fireHeigth = 40
+const fireColorsPalette = [{ "r": 7, "g": 7, "b": 7 }, { "r": 31, "g": 7, "b": 7 }, { "r": 47, "g": 15, "b": 7 }, { "r": 71, "g": 15, "b": 7 }, { "r": 87, "g": 23, "b": 7 }, { "r": 103, "g": 31, "b": 7 }, { "r": 119, "g": 31, "b": 7 }, { "r": 143, "g": 39, "b": 7 }, { "r": 159, "g": 47, "b": 7 }, { "r": 175, "g": 63, "b": 7 }, { "r": 191, "g": 71, "b": 7 }, { "r": 199, "g": 71, "b": 7 }, { "r": 223, "g": 79, "b": 7 }, { "r": 223, "g": 87, "b": 7 }, { "r": 223, "g": 87, "b": 7 }, { "r": 215, "g": 95, "b": 7 }, { "r": 215, "g": 95, "b": 7 }, { "r": 215, "g": 103, "b": 15 }, { "r": 207, "g": 111, "b": 15 }, { "r": 207, "g": 119, "b": 15 }, { "r": 207, "g": 127, "b": 15 }, { "r": 207, "g": 135, "b": 23 }, { "r": 199, "g": 135, "b": 23 }, { "r": 199, "g": 143, "b": 23 }, { "r": 199, "g": 151, "b": 31 }, { "r": 191, "g": 159, "b": 31 }, { "r": 191, "g": 159, "b": 31 }, { "r": 191, "g": 167, "b": 39 }, { "r": 191, "g": 167, "b": 39 }, { "r": 191, "g": 175, "b": 47 }, { "r": 183, "g": 175, "b": 47 }, { "r": 183, "g": 183, "b": 47 }, { "r": 183, "g": 183, "b": 55 }, { "r": 207, "g": 207, "b": 111 }, { "r": 223, "g": 223, "b": 159 }, { "r": 239, "g": 239, "b": 199 }, { "r": 255, "g": 255, "b": 255 }]
+let alter = true
+
+createFireDataStructure()
+createFireSouce()
+renderFire()
+
+function start(){
+    createFireSouce()
+    renderFire()
+
+    setInterval(calculateFirePropagation, 30)
+}
+
+//Cria o vetor
+function createFireDataStructure(){
+    const numberOfPixels = fireWidth * fireHeigth
+
+    for(let i = 0; i < numberOfPixels; i++){
+        firePixelsArray[i]=0;
+    }
+}
+
+//calcula a propagação do fogo
+function calculateFirePropagation(){
+    for(let column=0; column < fireWidth; column++){
+        for(let row = 0; row < fireHeigth; row++){
+            const pixelIndex = column + ( fireWidth * row )
+
+            updatefireIntensityPerPixel(pixelIndex)
+        }
+    }
+    renderFire()
+}
+
+//atualiza a intensidade do fogo
+function updatefireIntensityPerPixel(currentPixelIndex){
+    const belowPixelIndex = currentPixelIndex + fireWidth
+
+    if(belowPixelIndex >= fireWidth * fireHeigth){
+        return
+    }
+
+    const decay = Math.floor(Math.random() * 3)
+    const belowPixelFireIntensity = 
+    //belowPixelIndex + fireWidth < firePixelsArray.length ?
+    firePixelsArray[belowPixelIndex]// : firePixelsArray[firePixelsArray.length-1]
+    const newFireIntesity = 
+    belowPixelFireIntensity - decay >=0 ? belowPixelFireIntensity - decay : 0
+    if(alter)
+    {firePixelsArray[currentPixelIndex + decay] = newFireIntesity}
+    else 
+    {firePixelsArray[currentPixelIndex - decay] = newFireIntesity}
+}
+
+function renderFire() {
+    const debug = false;
+    let html = '<table cellpadding=0 cellspacing=0>'
+
+    for(let row = 0; row < fireHeigth; row++) {
+        html +='<tr>'
+        for(let column = 0; column< fireWidth; column++){
+            const pixelIndex = column + (fireWidth * row)
+            const fireIntensity = firePixelsArray[pixelIndex]
+
+            if(debug === true){
+        
+                html += '<td>'
+                html += `<div class="pixel-index">${pixelIndex}</div>`
+                html += fireIntensity
+                html +='</td>'
+
+            }else{
+                const color = fireColorsPalette[fireIntensity]
+                const colorString = `${color.r}, ${color.g}, ${color.b}`
+                 html +=`<td class="pixel" style="background-color: rgb(${colorString})">`
+                 html +=`</td>`
+            }
+
+        }
+
+        html +='</tr>'
+    }
+
+    html +='</table>'
+
+    document.querySelector('#fireCanvas').innerHTML = html
+}
+
+//Dá valor para a ultima linha
+function createFireSouce(temp = 36){
+    for(let column = 0; column <= fireWidth; column++){
+        const overflowPixelIndex = fireWidth * fireHeigth
+        const pixelIndex = (overflowPixelIndex - fireWidth) + column
+
+        firePixelsArray[pixelIndex] = temp
+    }
+}
+
+
+function alterWind(){
+  
+    alter ?
+    alter = false : alter = true
+}
+
+function startFire(){
+    start()
+}
+
+function plusFire(){
+    let temp = 
+    firePixelsArray[firePixelsArray.length-1] + 12 <= 36 ?
+    firePixelsArray[firePixelsArray.length-1] + 12 : 36 
+    
+    createFireSouce(temp)
+}
+
+function lessFire(){
+    let temp = 
+
+    firePixelsArray[firePixelsArray.length-1] - 12 >= 0 ?
+    firePixelsArray[firePixelsArray.length-1] - 12 : 0 
+    
+    createFireSouce(temp)
+}
+
+function endFire(){
+    createFireSouce(0)
+}
